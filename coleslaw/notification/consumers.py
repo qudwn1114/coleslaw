b0f90834b1fc170a6881f3cc8e65cfb39de6d84f
qudwn1114 <qudwn1114@gmail.com>
2024-06-16 04:00:21 +0900

    fix: consumers.py

--- coleslaw/notification/consumers.py
+++ coleslaw/notification/consumers.py
@@ -1,13 +1,17 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
+from system_manage.models import Shop
 
 import json
 
-class OrderConsumer(AsyncWebsocketConsumer):
+class OrderConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         try:
             self.shop_id = self.scope['url_route']['kwargs']['shop_id']
             self.group_name = f"shop_order_{self.shop_id}"
+
+            if not await self.check_shop_exists(self.shop_id): # 가맹점체크
+                raise ValueError('가맹점이 존재하지 않습니다.')
 
             # send 등 과 같은 동기적인 함수를 비동기적으로 사용하기 위해서는 async_to_sync 로 감싸줘야함
             # 현재 채널을 그룹에 추가합니다. 
@@ -16,8 +20,7 @@ class OrderConsumer(AsyncWebsocketConsumer):
                 self.channel_name
             )
             await self.accept()
-        except ValueError as e: # 값 오류가 있을 경우 (예: 방이 존재하지 않음), 오류 메시지를 보내고 연결을 종료합니다.
-            await self.send_json({'error': str(e)})
+        except ValueError as e: # 값 오류가 있을 경우 연결을 종료합니다.
             await self.close()
 
     async def disconnect(self, close_code):
@@ -50,3 +53,8 @@ class OrderConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message,
         }))
+    
+    @database_sync_to_async
+    def check_shop_exists(self, shop_id):
+        # 주어진 ID로 채팅방이 존재하는지 확인합니다.
+        return Shop.objects.filter(id=shop_id).exists()
