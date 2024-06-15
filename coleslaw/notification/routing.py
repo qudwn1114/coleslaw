4bacc0db959b30e72285a43cf0286c68045709fd
qudwn1114 <qudwn1114@gmail.com>
2024-06-16 02:23:12 +0900

    feat: django channels

--- coleslaw/notification/routing.py
+++ coleslaw/notification/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path, path
 from notification import consumers
 
 websocket_urlpatterns = [
-    path('ws/shop/<int:shop_id>/', consumers.NotificationConsumer.as_asgi()),
+    path('ws/shop/<int:shop_id>/order/', consumers.OrderConsumer.as_asgi()),
 ]
