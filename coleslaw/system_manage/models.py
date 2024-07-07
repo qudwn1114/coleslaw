1bb5728f45c2862e5c6d6b98befd6abbc754c8e7
qudwn1114 <qudwn1114@gmail.com>
2024-07-07 19:28:06 +0900

    chore: add column

--- coleslaw/system_manage/models.py
+++ coleslaw/system_manage/models.py
@@ -84,20 +84,6 @@ class ShopAdmin(models.Model):
         ]
         db_table = 'shop_admin'
 
-
-class ShopPersonType(models.Model):
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    person_type = models.ForeignKey(PersonType, on_delete=models.CASCADE)
-    description = models.CharField(default='', max_length=255, verbose_name='설명')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
-
-    class Meta :
-        constraints = [
-            models.UniqueConstraint(fields=['shop', 'person_type'], name='shop_person_type_unique')
-        ]
-        db_table = 'shop_person_type'
-
 # shop table
 class ShopTable(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
@@ -204,6 +190,21 @@ class GoodsOptionDetail(models.Model):
     
     class Meta:
         db_table='goods_option_detail'
+
+
+class ShopPersonType(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    person_type = models.ForeignKey(PersonType, on_delete=models.CASCADE)
+    description = models.CharField(default='', max_length=255, verbose_name='설명')
+    goods = models.ForeignKey(Goods, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['shop', 'person_type'], name='shop_person_type_unique')
+        ]
+        db_table = 'shop_person_type'
         
 class EntryQueue(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
