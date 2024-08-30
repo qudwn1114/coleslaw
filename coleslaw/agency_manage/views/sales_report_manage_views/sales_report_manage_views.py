3fbd320672d106a3a72591d6dae92c9b50fc14ce
qudwn1114 <qudwn1114@gmail.com>
2024-08-30 17:48:59 +0900

    fix: sales report

--- coleslaw/agency_manage/views/sales_report_manage_views/sales_report_manage_views.py
+++ coleslaw/agency_manage/views/sales_report_manage_views/sales_report_manage_views.py
@@ -6,7 +6,7 @@ from django.http.response import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 from django.utils.decorators import method_decorator
 from django.utils import timezone
-from django.db.models import CharField, F, Value as V, Func, Case, When, Prefetch, Sum
+from django.db.models import CharField, F, Value as V, Func, Case, When, Prefetch, Sum, Q
 
 from system_manage.utils import ResponseToXlsx
 from system_manage.decorators import permission_required
@@ -58,40 +58,79 @@ class AgencySalesReportManage(View):
         else:
             days = calendar.monthrange(year=year, month=month)[1]
 
-        shop_list = Shop.objects.filter(agency=agency).values('id', 'name_kr').order_by('name_kr')
+        shop_list = Shop.objects.filter(agency=agency).annotate(
+            card=V(0),
+            cash1=V(0),
+            cash2=V(0),
+            total=V(0),
+        ).values('id', 'name_kr', 'card', 'cash1', 'cash2', 'total').order_by('name_kr')
         sales_report_list = []
+
+        sum_card_price = 0
+        sum_cash1_price = 0
+        sum_cash2_price = 0
         sum_total_price = 0
+
         if shop_list:
             for i in range(1,  days+1):
-                d = datetime.datetime.strptime(f"{year_month}-{format(i, '02')}", '%Y-%m-%d')
+                d = f"{year_month}-{format(i, '02')}"
                 filter_dict['status']=True
+                filter_dict['amount__gt']=0
                 filter_dict['order__date'] = d
-                queryset = OrderPayment.objects.filter(**filter_dict).values(
+                queryset = OrderPayment.objects.filter(**filter_dict).annotate(
+                    paymentMethod = Case(
+                        When(payment_method='0', then=V('0')),
+                        When(Q(payment_method='1') & Q(approvalNumber=''), then=V('1')),
+                        When(Q(payment_method='1'), then=V('1-1')),
+                        default=V('0'), output_field=CharField()
+                    ),
+                ).values(
                     'order__shop_id',
-                    'payment_method',
+                    'paymentMethod',
                 ).annotate(
                     total_sale_price=Sum("amount")
                 ).order_by('order__shop_id')
+                day_total = 0 
+                day_card = 0
+                day_cash1 = 0
+                day_cash2 = 0
                 for j in shop_list:
                     new_queryset = queryset.filter(order__shop_id=j['id'])
                     total = 0 
                     card = 0
-                    cash = 0
+                    cash1 = 0
+                    cash2 = 0
                     for k in new_queryset:
-                        if k['payment_method'] == '0':
+                        if k['paymentMethod'] == '0':
                             card = k['total_sale_price']
-                        elif k['payment_method'] == '1':
-                            cash = k['total_sale_price']
-                        total += k['total_sale_price']
-                    sales_report_list.append({"date":d, "name_kr": j['name_kr'], "card":card, "cash": cash, "total": total})
+                            day_card += card
+                            j['card'] += card
+                        elif k['paymentMethod'] == '1':
+                            cash1 = k['total_sale_price']
+                            day_cash1 += cash1
+                            j['cash1'] += cash1
+                        elif k['paymentMethod'] == '1-1':
+                            cash2 = k['total_sale_price']
+                            day_cash2 += cash2
+                            j['cash2'] += cash2
+                        total = k['total_sale_price']
+                        day_total += total
+                        j['total'] += total
+                    sales_report_list.append({"date":d, "name_kr": j['name_kr'], "card":card, "cash1": cash1, "cash2": cash2, "total": total})
+                sales_report_list.append({"date":"", "name_kr":"합계", "card":day_card, "cash1":day_cash1, "cash2": day_cash2, "total":day_total}) 
 
-                    sum_total_price += total
+            for i in shop_list:
+                sum_card_price += i['card']
+                sum_cash1_price += i['cash1']
+                sum_cash2_price += i['cash2']
+                sum_total_price += i['total']
+                sales_report_list.append({"date":year_month, "name_kr":i['name_kr'], "card":i['card'], "cash1":i['cash1'], "cash2": i['cash2'], "total":i['total']}) 
 
-                sales_report_list.append({"date":"", "name_kr":"", "card":"", "cash":"", "total":""})
-        
+            sales_report_list.append({"date":"", "name_kr":"전체합계", "card":sum_card_price, "cash1":sum_cash1_price, "cash2":sum_cash2_price, "total":sum_total_price}) 
+
         if excel:
             filename = f"{agency.name} {year_month} 가맹점 별 매출"
-            headers = ['일자별', '가맹점', '카드매출', '현금매출', '합계']
+            headers = ['일자별', '가맹점', '카드매출', '현금매출(O)', '현금매출(X)', '합계']
             response = HttpResponse(content_type='application/ms-excel')
             response['Content-Disposition'] = 'attachment; filename*=UTF-8\'\'%s.xlsx' % urllib.parse.quote(filename.encode('utf-8'))
             wb = Workbook()
@@ -101,7 +140,7 @@ class AgencySalesReportManage(View):
             ws.append(headers)
 
             for i in sales_report_list:
-                ws.append([i["date"], i["name_kr"], i['card'], i['cash'], i['total']])
+                ws.append([i["date"], i["name_kr"], i['card'], i['cash1'], i['cash2'], i['total']])
             wb.save(response)
             return response
 
