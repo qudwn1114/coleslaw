9fbe18950f86bbb9bafeb786dc623f22695f5974
qudwn1114 <qudwn1114@gmail.com>
2024-09-02 20:37:14 +0900

    feat: sales report

--- coleslaw/agency_manage/views/sales_report_manage_views/sales_report_manage_views.py
+++ coleslaw/agency_manage/views/sales_report_manage_views/sales_report_manage_views.py
@@ -68,13 +68,15 @@ class AgencySalesReportManage(View):
         
         shop_list =shop.annotate(
             card=V(0),
+            cash=V(0),
             cash1=V(0),
             cash2=V(0),
             total=V(0),
-        ).values('id', 'name_kr', 'card', 'cash1', 'cash2', 'total').order_by('name_kr')
+        ).values('id', 'name_kr', 'card', 'cash', 'cash1', 'cash2', 'total').order_by('name_kr')
         sales_report_list = []
 
         sum_card_price = 0
+        sum_cash_price = 0
         sum_cash1_price = 0
         sum_cash2_price = 0
         sum_total_price = 0
@@ -100,12 +102,14 @@ class AgencySalesReportManage(View):
                 ).order_by('order__shop_id')
                 day_total = 0 
                 day_card = 0
+                day_cash = 0
                 day_cash1 = 0
                 day_cash2 = 0
                 for j in shop_list:
                     new_queryset = queryset.filter(order__shop_id=j['id'])
                     total = 0 
                     card = 0
+                    cash = 0
                     cash1 = 0
                     cash2 = 0
                     for k in new_queryset:
@@ -115,33 +119,42 @@ class AgencySalesReportManage(View):
                             j['card'] += card
                         elif k['paymentMethod'] == '1':
                             cash1 = k['total_sale_price']
+
+                            day_cash+=cash1
                             day_cash1 += cash1
                             j['cash1'] += cash1
+                            cash += cash1
+                            j['cash'] += cash1
                         elif k['paymentMethod'] == '1-1':
                             cash2 = k['total_sale_price']
+
+                            day_cash+=cash2
                             day_cash2 += cash2
                             j['cash2'] += cash2
+                            cash += cash2
+                            j['cash'] += cash2
 
                         _total = k['total_sale_price']
                         total += _total
                         day_total += _total
                         j['total'] += _total
 
-                    sales_report_list.append({"date":d, "name_kr": j['name_kr'], "card":card, "cash1": cash1, "cash2": cash2, "total": total})
-                sales_report_list.append({"date":"", "name_kr":"합계", "card":day_card, "cash1":day_cash1, "cash2": day_cash2, "total":day_total}) 
+                    sales_report_list.append({"date":d, "name_kr": j['name_kr'], "card":card, "cash":cash, "cash1": cash1, "cash2": cash2, "total": total})
+                sales_report_list.append({"date":"", "name_kr":"합계", "card":day_card, "cash":day_cash, "cash1":day_cash1, "cash2": day_cash2, "total":day_total}) 
 
             for i in shop_list:
                 sum_card_price += i['card']
+                sum_cash_price += i['cash']
                 sum_cash1_price += i['cash1']
                 sum_cash2_price += i['cash2']
                 sum_total_price += i['total']
-                sales_report_list.append({"date":"전체기간", "name_kr":i['name_kr'], "card":i['card'], "cash1":i['cash1'], "cash2": i['cash2'], "total":i['total']}) 
+                sales_report_list.append({"date":"전체기간", "name_kr":i['name_kr'], "card":i['card'], "cash":i['cash'], "cash1":i['cash1'], "cash2": i['cash2'], "total":i['total']}) 
 
-            sales_report_list.append({"date":"", "name_kr":"전체합계", "card":sum_card_price, "cash1":sum_cash1_price, "cash2":sum_cash2_price, "total":sum_total_price}) 
+            sales_report_list.append({"date":"", "name_kr":"전체합계", "card":sum_card_price,"cash":sum_cash_price, "cash1":sum_cash1_price, "cash2":sum_cash2_price, "total":sum_total_price}) 
 
         if excel:
             filename = f"{agency.name} {date_name} 가맹점 별 매출"
-            headers = ['일자별', '가맹점', '카드매출', '현금매출(O)', '현금매출(X)', '합계']
+            headers = ['일자별', '가맹점', '카드매출', '현금매출', '현금매출(O)', '현금매출(X)', '합계']
             response = HttpResponse(content_type='application/ms-excel')
             response['Content-Disposition'] = 'attachment; filename*=UTF-8\'\'%s.xlsx' % urllib.parse.quote(filename.encode('utf-8'))
             wb = Workbook()
@@ -151,7 +164,7 @@ class AgencySalesReportManage(View):
             ws.append(headers)
 
             for i in sales_report_list:
-                ws.append([i["date"], i["name_kr"], i['card'], i['cash1'], i['cash2'], i['total']])
+                ws.append([i["date"], i["name_kr"], i['card'],i['cash'], i['cash1'], i['cash2'], i['total']])
             wb.save(response)
             return response
 
